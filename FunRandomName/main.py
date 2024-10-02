04a6011df9599a09292106a4559649ffc1322605
Prox-1 <kurganovdanka@gmail.com>
2024-10-02 18:05:16 +0300

    version 2.0: the class was recreated, a gender test was written

--- FunRandomName/main.py
+++ FunRandomName/main.py
@@ -2,113 +2,109 @@ import random
 
 
 class FunRandomName:
-    __funny_adjectives = {
-        "Чудесный": 0,
-        "Неприличный": 0,
-        "Бесполезный": 0,
-        "Забавный": 0,
-        "Облачный": 0,
-        "Смешной": 0,
-        "Безумный": 0,
-        "Вкусный": 0,
-        "Мягкий": 0,
-        "Пухлый": 0,
-        "Дразнящий": 0,
-        "Шумный": 0,
-        "Фантастический": 0,
-        "Игривый": 0,
-        "Поджаренный": 0,
-        "Пурпурный": 0,
-        "Прыгучий": 0,
-        "Радужный": 0,
-        "Сверкающий": 0,
-        "Кривой": 0,
+    __funny_adjectives = {  # [пол, откат]
+        "Чудесный": [0, 0],
+        "Неприличная": [1, 0],
+        "Бесполезный": [0, 0],
+        "Забавная": [1, 0],
+        "Облачный": [0, 0],
+        "Смешная": [1, 0],
+        "Безумный": [0, 0],
+        "Вкусная": [1, 0],
+        "Мягкий": [0, 0],
+        "Пухлая": [1, 0],
+        "Дразнящий": [0, 0],
+        "Шумная": [1, 0],
+        "Фантастический": [0, 0],
+        "Игривая": [1, 0],
+        "Поджаренный": [0, 0],
+        "Пурпурная": [1, 0],
+        "Прыгучий": [0, 0],
+        "Радужная": [1, 0],
+        "Сверкающий": [0, 0],
+        "Кривая": [1, 0],
     }
+
     __names = {
-        "слон слониха": 0,
-        "тигр тигрица": 0,
-        "медведь медведица": 0,
-        "бобр бобриха": 0,
-        "козел коза": 0,
-        "крокодил утка": 0,
-        "горилла обезьяна": 0,
-        "единорог оса": 0,
-        "кит пиранья": 0,
-        "дельфин акула": 0,
-        "волк волчица": 0,
-        "лис лиса": 0,
-        "голубь птица": 0,
-        "муравей букашка": 0,
-        "кот кошка": 0,
-        "хорек свинка": 0,
-        "змей змея": 0,
-        "ленивец коала": 0,
-        "сервал зебра": 0,
-        "кенгуру грибница": 0,
+        "кот": [0, 0],
+        "пес": [0, 0],
+        "кролик": [0, 0],
+        "конь": [0, 0],
+        "попугай": [0, 0],
+        "жираф": [0, 0],
+        "слон": [0, 0],
+        "тигрица": [1, 0],
+        "медведь": [0, 0],
+        "овца": [1, 0],
+        "лиса": [1, 0],
+        "коза": [1, 0],
+        "жук": [0, 0],
+        "курица": [1, 0],
+        "бобриха": [1, 0],
+        "петух": [0, 0],
+        "медуза": [1, 0],
+        "акула": [1, 0],
+        "ара": [1, 0],
+        "верблюд": [0, 0],
     }
 
-    # Назначение отката на использованные значения прилагательных и имен
-    def __rollback(self, adj, name):
-        FunRandomName.__funny_adjectives[adj] = 12
-        FunRandomName.__names[name] = 12
+    def __get_gender(self):
+        return random.randint(0, 1)
 
-    # Уменьшение отката при создании нового имени
+    def __create_available_list(self, gender: int, dict_: dict):
+        return [
+            key
+            for key in list(dict_.keys())
+            if dict_[key][0] == gender and dict_[key][1] == 0
+        ]
+
+    def __rollback(self, adjective: str, name: str):
+        FunRandomName.__funny_adjectives[adjective][1] = 7
+        FunRandomName.__names[name][1] = 7
+
     def __rollback_reductions(self, dict_: dict):
-        def rollback_reduction(value):
-            return value - 1 if value != 0 else value
+        def rollback_reduction(value: tuple):
+            if value[1] != 0:
+                value[1] -= 1
+            return value
 
         reduced_dict = dict(
             map(lambda item: (item[0], rollback_reduction(item[1])), dict_.items())
         )
         return reduced_dict
 
-    # Функция которую вставим в filter() для отбрасывания значений у которых есть откат
-    def __filter_func(self, x):
-        if x in list(FunRandomName.__funny_adjectives.keys()):  # костыль
-            dict_ = FunRandomName.__funny_adjectives
-        else:
-            dict_ = FunRandomName.__names
+    def __change_random_elem(self, list_: list):
+        return list_[random.randint(0, len(list_) - 1)]
 
-        if dict_[x] == 0:
-            return True
-        else:
-            False
-
-    # Создание списка значений
-    def __create_lists_of_available_values(self):
-        available_list_adjs = filter(
-            self.__filter_func, FunRandomName.__funny_adjectives
-        )
-        available_list_names = filter(self.__filter_func, FunRandomName.__names)
-        return list(available_list_adjs), list(available_list_names)
-
-    # Получение элемента из прилагательных
-    def __get_elem_adj(self, list_: list, n: int):
+    def __create_funny_name(self, adjectives_list: list, names_list: list):
         FunRandomName.__funny_adjectives = self.__rollback_reductions(
             FunRandomName.__funny_adjectives
         )
-        return list_[n]
-
-    # Получение элемента из имен
-    def __get_elem_name(self, list_: list, n: int):
         FunRandomName.__names = self.__rollback_reductions(FunRandomName.__names)
-        return list_[n]
-
-    def create_funny_name(self):
-        available_list_adjs, available_list_names = (
-            self.__create_lists_of_available_values()
+        return self.__change_random_elem(adjectives_list), self.__change_random_elem(
+            names_list
         )
-        l_names = len(available_list_names)
-        l_adjs = len(available_list_adjs)
-        n_of_random_name = random.randint(0, l_names - 1)
-        n_of_random_adj = random.randint(0, l_adjs - 1)
-        adj = self.__get_elem_adj(available_list_adjs, n_of_random_adj)
-        name = self.__get_elem_name(available_list_names, n_of_random_name)
-        self.__rollback(adj, name)
-        if random.randint(0, 1) == 1:
-            return f"{adj[:-2]+'ая'} {name.split()[1]}"
-        else:
-            return f"{adj} {name.split()[0]}"
 
+    def __check_gender(self, gender):
+        try:
+            if gender == "random":
+                gender = self.__get_gender()
+            elif gender == "male":
+                gender = 0
+            elif gender == "female":
+                gender = 1
+            else:
+                raise ValueError("Invalid gender value provided.")
+        except ValueError:
+            print('Введите значение gender из ["random", "male", "female"]')
+        return gender
 
-print(FunRandomName().create_funny_name())
+    def get_funny_name(self, gender="random"):
+        gender = self.__check_gender(gender)
+        adjectives_list = self.__create_available_list(
+            gender, FunRandomName.__funny_adjectives
+        )
+        names_list = self.__create_available_list(gender, FunRandomName.__names)
+        adjective, name = self.__create_funny_name(adjectives_list, names_list)
+        self.__rollback(adjective, name)
+        return f"{adjective} {name}"
--- FunRandomName/test_main.py
+++ FunRandomName/test_main.py
@@ -6,9 +6,9 @@ from main import FunRandomName
 def test_names_rollback():
     names_rollback_list = []
     for i in range(1, 2225):
-        if i > 12:
+        if i > 7:
             names_rollback_list.pop(0)
-        new_value = (FunRandomName().create_funny_name()).split(" ")[-1]
+        new_value = (FunRandomName().get_funny_name()).split(" ")[-1]
         assert new_value not in names_rollback_list
         names_rollback_list.append(new_value)
 
@@ -17,9 +17,9 @@ def test_names_rollback():
 def test_funny_adjectives_rollback():
     funny_adjectives_rollback_list = []
     for i in range(1, 2225):
-        if i > 12:
+        if i > 7:
             funny_adjectives_rollback_list.pop(0)
-        new_value = (FunRandomName().create_funny_name()).split(" ")[0]
+        new_value = (FunRandomName().get_funny_name()).split(" ")[0]
         assert new_value not in funny_adjectives_rollback_list
         funny_adjectives_rollback_list.append(new_value)
 
@@ -28,8 +28,20 @@ def test_funny_adjectives_rollback():
 def test_funny_names_rollback():
     funny_names_rollback_list = []
     for i in range(1, 2225):
-        if i > 12:
+        if i > 7:
             funny_names_rollback_list.pop(0)
-        new_value = FunRandomName().create_funny_name()
+        new_value = FunRandomName().get_funny_name()
         assert new_value not in funny_names_rollback_list
         funny_names_rollback_list.append(new_value)
+
+
+# Тест на корректную работу с явным обозначением гендера
+def test_with_some_gender():
+    for i in range(1, 300):
+        new_value = FunRandomName().get_funny_name(gender="female").split(" ")[0]
+        assert new_value[-2:] == "ая"
+    for i in range(1, 300):
+        new_value = FunRandomName().get_funny_name(gender="male").split(" ")[0]
+        assert new_value[-2:] != "ая"
+    with pytest.raises(ValueError):
+        new_value = FunRandomName().get_funny_name(gender=2)
